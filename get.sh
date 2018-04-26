5f4b36927facfeed8f9bc16b5b4377e82dcf5727
Ying Huang <yhuang@imvu.com>
2018-04-25 22:21:19 -0700

    n

--- get.sh
+++ get.sh
@@ -5,5 +5,5 @@ cd "$SCRIPTPATH"
 
 leetcode submission 1 -o submissions
 ~/loadrc/bashrc/jformat.sh
-jdupes -1dNr submissions/
 autopep8 --in-place --aggressive --aggressive -r submissions/
+jdupes -1dNr submissions/

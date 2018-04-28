3136d814824649f00656bbb9e1f4d565466e2922
Ying Huang <yhuang@imvu.com>
2018-04-28 14:52:03 -0700

    n

---
+++ get_tags.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+./build.sh
+leetcode user -l
+
+doTag () {
+    leetcode list -q L -t "$1" > tags/"$1".md
+}
+
+while read -r line || [[ -n "$line" ]]
+do
+    doTag "$line"
+done < companies.list
+
+while read -r line || [[ -n "$line" ]]
+do
+    doTag "$line"
+done < tags.list

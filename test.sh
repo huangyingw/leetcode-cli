e6c933750aba41f939bc47a36d0745085e82e2bc
Ying Huang <yhuang@imvu.com>
2018-04-30 10:44:09 -0700

    n

--- test.sh
+++ test.sh
@@ -4,5 +4,5 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 leetcode test \
-    /Users/huangyingw/Dropbox/myproject/git/interview/skygragon/leetcode-cli/show/104.maximum-depth-of-binary-tree.leetcode.py \
+    ./submissions/104.maximum-depth-of-binary-tree.152193284.Wrong-Answer.leetcode.py \
     -t '[1,2,3,4,null,null,5]'

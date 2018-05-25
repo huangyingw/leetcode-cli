027d00372595e697f8d8aacfadd44673b7612001
Ying Huang <yhuang@imvu.com>
2018-05-25 10:44:40 -0700

    n

---
+++ restore.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+rsync -aHv --progress --delete \
+    --exclude-from=./excludeFile \
+    ../leetcode-cli.bak/ \
+    ./

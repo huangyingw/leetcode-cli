30bfc0459abadf5ceffaa75e1851ca1f4f23e54f
Ying Huang <yhuang@imvu.com>
2018-06-11 11:06:04 -0700

    n

--- commit.sh
+++ commit.sh
@@ -3,9 +3,9 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-~/loadrc/gitrc/grsh.sh
 ./backup.sh
 ./remove_dup.sh
+git co ./submissions/
 git add ./submissions/
 ~/loadrc/gitrc/g.sh
 ./restore.sh

5d73d9b03ccbbb81da382bc5d0be07e2721d0adf
Ying Huang <yhuang@imvu.com>
2018-06-16 16:48:39 -0700

    n

--- remove_dup.sh
+++ remove_dup.sh
@@ -5,5 +5,5 @@ cd "$SCRIPTPATH"
 
 ~/loadrc/bashrc/jformat.sh
 autopep8 --in-place -r submissions/
-find . -type f -name \*.orig -delete
+find . -type f \( -name \*.orig -o -name \*.bak \) -delete
 jdupes -1dNr submissions/

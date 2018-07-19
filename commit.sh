1fcd8fc6eff3b2cc2dbdd30edfb24c9c85af67eb
Ying Huang <yhuang@imvu.com>
2018-07-19 16:09:08 -0700

    n

--- commit.sh
+++ commit.sh
@@ -4,12 +4,7 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 git co ./submissions/
-./remove_prints.sh
-~/loadrc/bashrc/jformat.sh
-autopep8 --in-place -r submissions/
-find . -type f \( -name \*.orig -o -name \*.bak \) -delete
-./backup.sh
-jdupes -1dNr submissions/
+./remove_dup.sh
 git add ./submissions/
 ~/loadrc/gitrc/g.sh
 ./restore.sh

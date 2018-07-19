03d710d6063f8dfbea48aef0929e4e2cb40045a8
Ying Huang <yhuang@imvu.com>
2018-07-18 21:15:11 -0700

    n

--- remove_prints.sh
+++ remove_prints.sh
@@ -3,11 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-resultFile="find_prints.sh.findresult"
-xargs grep -inH -- "print.*(" < ./files.proj | grep "./submissions/" | sed "s/:.*//g" | sort -u | tee "$resultFile"
-
-while read ss
-do
-    ss=$(echo $ss | sed  -e "s/\//\\\\\//g")
-    sed -i.bak "/$ss/d" "$resultFile"
-done < find_prints.ignore
+find ./submissions/ -type f -print0 | xargs -0 sed -i.bak '/print.*(/d'

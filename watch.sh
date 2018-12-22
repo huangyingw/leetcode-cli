28dceb9295458b86c3f66eddb9b5c74db0b6dad9
Ying Huang <yhuang@imvu.com>
2018-12-22 07:34:47 -0800

    n

--- watch.sh
+++ watch.sh
@@ -3,4 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-/usr/bin/watch ~/loadrc/bashrc/check_running.sh ./submission.sh
+~/loadrc/bashrc/check_running.sh ./submission.sh

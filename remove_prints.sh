593fa8ca75f652c9d806c3d890d081c8697cf879
Ying Huang <yhuang@imvu.com>
2019-02-03 08:04:22 -0800

    n

--- remove_prints.sh
+++ remove_prints.sh
@@ -4,6 +4,4 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 cd ./submissions && \
-    git stash && \
-    find . -type f -print0 | xargs -0 sed -i.bak '/print.*(/d;${/^$/d;}' && \
-    git stash pop
+    find . -type f -print0 | xargs -0 sed -i.bak '/print.*(/d;${/^$/d;}'

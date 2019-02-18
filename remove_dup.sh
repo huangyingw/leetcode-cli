fd13caddc11942811f48b61575e27b85df3b3c90
Ying Huang <yhuang@imvu.com>
2019-02-17 23:07:51 -0800

    n

--- remove_dup.sh
+++ remove_dup.sh
@@ -3,6 +3,14 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+cd ./submissions
+if [ -n "$(git status --porcelain)" ]
+then
+    echo -e "${red}the git repository is unclean, please check it before continuing... ${NC}"
+    exit 1
+fi
+cd -
+
 ./remove_prints.sh
 ~/loadrc/bashrc/jformat.sh
 autopep8 --in-place -r submissions/

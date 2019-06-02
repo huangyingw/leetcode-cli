13bdbebd18c33d3b4b4fa0dde064865be7d82a0a
Ying Huang <huangyingw@gmail.com>
2019-06-02 15:42:54 -0700

    n

--- submit.test.sh
+++ submit.test.sh
@@ -4,4 +4,8 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 git co test/1001_Grid_Illumination.py 
-leetcode submit -d ./test/1001_Grid_Illumination.py
+git co test/1342343283_Grid_Illumination.py 
+git co submissions/101.symmetric-tree.204254047.Wrong-Answer.leetcode.py
+leetcode submit -d ./test/1001_Grid_Illumination.py 
+leetcode submit -d ./test/1342343283_Grid_Illumination.py 
+leetcode submit -d ./submissions/101.symmetric-tree.204254047.Wrong-Answer.leetcode.py

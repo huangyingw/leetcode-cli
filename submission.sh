5dcd1d1990a11ca75c3511eb3eadd459f207d08b
Ying Huang <huangyingw@gmail.com>
2019-06-03 22:34:14 -0700

    n

--- submission.sh
+++ submission.sh
@@ -3,16 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-./build.sh
-./restore.sh
-
-doGet () {
-    leetcode user -l
-    leetcode submission "$1" -o submissions
-}
-
-while read -r line || [[ -n "$line" ]]
-do
-    doGet "$line"
-done < problems.list
-./remove_dup.sh
+leetcode submission -a -o submissions

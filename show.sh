975b7a5afe52593bce7272d6990faebd04da1fb3
Ying Huang <huangyingw@gmail.com>
2019-06-05 21:04:17 -0700

    n

--- show.sh
+++ show.sh
@@ -4,15 +4,5 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 ./build.sh
-rm  ./show/*.py
-rm -fr ~/.lc/leetcode/cache/*.json
 
-doShow () {
-    leetcode show "$1" -gx -l python -o ./show/
-}
-
-while read -r line || [[ -n "$line" ]]
-do
-    doShow "$line"
-done < problems.list
-autopep8 --in-place -r show/
+leetcode show -a -gx -l python -o ./show/

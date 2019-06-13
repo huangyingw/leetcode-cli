7ef28dd5be592a0358ea9a47497e890e93fde01a
Ying Huang <huangyingw@gmail.com>
2019-06-12 22:59:04 -0700

    n

--- show.sh
+++ show.sh
@@ -3,4 +3,6 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+find ~/.lc -type f -name problems.json -delete
+rm ./show/*.py
 leetcode show -a -gx -l python -o ./show/

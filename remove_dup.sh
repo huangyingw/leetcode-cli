f7f9c748e7d5e40bcbdcb74c696b89c8d6bca0bd
Ying Huang <huangyingw@gmail.com>
2019-07-07 22:09:39 -0700

    n

--- remove_dup.sh
+++ remove_dup.sh
@@ -3,6 +3,12 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
+if [ $(uname) == "Darwin" ]
+then
+    echo -e "${red}It could only run in Linux... ${NC}"
+    exit 1
+fi
+
 cd ./submissions
 git diff --quiet || exit 1
 cd -

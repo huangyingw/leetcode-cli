76803b13d1a051adcf3443c6e0835a5bb0be47ac
Ying Huang <huangyingw@gmail.com>
2019-09-17 09:14:00 -0700

    n

--- sort_accepts.py
+++ sort_accepts.py
@@ -3,13 +3,12 @@ import re
 
 fileName = 'files.proj'
 lines = [line.rstrip('\n') for line in open(fileName) if 'Accepted' in line]
-print('\n'.join(map(str, lines)))
 
 problem_accepts = {}
 for line in lines:
     num = re.findall(r'submissions\/(\d+)', line)[0]
     problem_accepts.setdefault(num, []).append(line)
 
-print(problem_accepts)
-problem_accepts = sorted(problem_accepts, key=lambda k: len(problem_accepts[k]), reverse=True)
-print(problem_accepts)
+for k in sorted(problem_accepts, key=lambda k: len(problem_accepts[k]), reverse=True):
+    for accepted in problem_accepts[k]:
+        print(accepted)

70cdd0acd38d4c44dae2756d89673deb170b9957
Ying Huang <huangyingw@gmail.com>
2019-09-29 20:52:20 -0700

    n

--- sort_non_accepts.py
+++ sort_non_accepts.py
@@ -6,15 +6,25 @@ myCmd = 'cd submissions/ ; git checkout \*Accepted\* ; cd -'
 os.system(myCmd)
 
 fileName = 'files.proj'
-lines = [line.rstrip('\n') for line in open(fileName) if 'Accepted' not in line]
+lines = [line.rstrip('\n') for line in open(fileName) if 'Accepted' in line]
 
 non_accepts = {}
+for line in lines:
+    num = re.findall(r'submissions\/(\d+)', line)[0]
+    non_accepts[num] = []
+
+
+lines = [line.rstrip('\n') for line in open(fileName) if 'Accepted' not in line]
+
 for line in lines:
     matches = re.findall(r'submissions\/(\d+)', line)
     if matches:
         num = matches[0]
         non_accepts.setdefault(num, []).append(line)
 
+
 for k in sorted(non_accepts, key=lambda k: len(non_accepts[k])):
+    if not non_accepts[k]:
+        print('"./submissions/%s.only_accepts.py"' % k)
     for non_accept in non_accepts[k]:
         print(non_accept)

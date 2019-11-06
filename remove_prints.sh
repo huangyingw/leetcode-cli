40f20c3c4016b3dd6bd87468d7a930156f6cef0b
Ying Huang <huangyingw@gmail.com>
2019-11-05 16:46:39 -0800

    n

--- remove_prints.sh
+++ remove_prints.sh
@@ -3,5 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-cd ./submissions && \
-    find . -type f -print0 | xargs -0 sed -i.bak '/print.*(/d;/^\s*$/d'
+find ./submissions/ -type f -print0 | xargs -0 sed -i.bak '/print.*(/d;/^\s*$/d'

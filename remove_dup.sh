2e1f83bfe165a410a67baaf36e25186725826b3a
Ying Huang <huangyingw@gmail.com>
2019-11-05 17:03:15 -0800

    n

--- remove_dup.sh
+++ remove_dup.sh
@@ -12,6 +12,6 @@ fi
 ./remove_prints.sh
 ~/loadrc/bashrc/jformat.sh
 autopep8 --in-place -r submissions/
-find . -type f -name \*.orig -delete
+find . -type f \( -name \*.orig -o -name \*.bak \) -delete
 ./backup.sh
 jdupes -1dNr submissions/

2ef37b134dff47cef86683cdb619837c9547842d
Ying Huang <huangyingw@gmail.com>
2019-11-26 21:30:49 -0800

    n

--- remove_dup.sh
+++ remove_dup.sh
@@ -3,12 +3,6 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-if [ $(uname) == "Darwin" ]
-then
-    echo -e "${red}It could only run in Linux... ${NC}"
-    exit 1
-fi
-
 ./remove_prints.sh
 ~/loadrc/bashrc/jformat.sh
 autopep8 --in-place -r submissions/

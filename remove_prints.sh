4af9760bb45769b3716fd69e062d9952f113354c
Ying Huang <huangyingw@gmail.com>
2020-01-13 21:38:58 -0800

    n

--- remove_prints.sh
+++ remove_prints.sh
@@ -3,4 +3,13 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-find ./submissions/ -type f -print0 | xargs -0 sed -i.bak '/print.*(/d;/^\s*$/d;/\bprint\b/d' 
+cd remove_comments
+~/loadrc/gitrc/grsh.sh
+find . -type f -name \*.py | while read ss
+do
+    ~/loadrc/pythonrc/remove_comments.py "$ss"
+    mv -fv "$ss.strip" "$ss"
+    autopep8 --in-place -r "$ss"
+    sed -i.bak '/print.*(/d;/^_author_/d;/^_project_/d;/\bprint\b/d;/^$/d;/^\s*$/d' "$ss"
+done
+git add .

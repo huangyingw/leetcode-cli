fd2faea21a065188eb5420e8a2fcad7361905354
Ying Huang <huangyingw@gmail.com>
2020-01-18 10:24:12 -0800

    n

--- remove_gabages.sh
+++ remove_gabages.sh
@@ -5,8 +5,13 @@ cd "$SCRIPTPATH"
 
 find ./submissions -type f -name \*.py | while read ss
 do
-    ~/loadrc/pythonrc/remove_comments.py "$ss"
-    mv -fv "$ss.strip" "$ss"
-    sed -i.bak '/print.*(/d;/^_author_/d;/__main__/d;/ = Solution()/d;/^_project_/d;/\bprint\b/d;s/#--//g;s/##//g;/^$/d;/^\s*$/d' "$ss"
     autopep8 --in-place "$ss"
+    if $(~/loadrc/pythonrc/remove_comments.py "$ss")
+    then
+        mv -fv "$ss.strip" "$ss"
+        sed -i.bak '/print.*(/d;/^_author_/d;/__main__/d;/ = Solution()/d;/^_project_/d;/\bprint\b/d;s/#--//g;s/##//g;/^$/d;/^\s*$/d' "$ss"
+        autopep8 --in-place "$ss"
+    else
+        mv -v "$ss" $(echo "$ss" | sed 's/submissions/remove_comments/g')
+    fi
 done

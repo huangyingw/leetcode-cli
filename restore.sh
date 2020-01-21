a7bc6bf15a607d3a13d3c6d8fd59366be118a272
Ying Huang <huangyingw@gmail.com>
2020-01-20 20:25:38 -0800

    n

--- restore.sh
+++ restore.sh
@@ -4,7 +4,7 @@ SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
 rsync -aHv --progress \
-    --delete \
+    --ignore-existing \
     --exclude-from=./excludeFile \
     ../leetcode-cli.orig/submissions/ \
     ./submissions/

c5121a2a5d8872d155aa6338523a1b21f6b83b93
Ying Huang <huangyingw@gmail.com>
2022-02-08 21:53:47 -0800

    n

---
+++ sort_exercises.sh
@@ -0,0 +1,14 @@
+#!/bin/zsh
+SCRIPT=$(realpath "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+cd "$SCRIPTPATH"
+
+~/loadrc/bashrc/mirror.sh
+cat files.proj | grep \/submissions\/.*.py | sed "s/\"//g" > files.proj.sort
+
+IFS=$'\r\n' GLOBIGNORE='*' command eval  'files=($(cat files.proj.sort))'
+
+for f in "${files[@]}"
+do
+    echo $(gstat -c %y -- "$f")$'\t'"$f"
+done | sort -nk1,1
